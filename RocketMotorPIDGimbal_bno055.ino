d72889d87eebbf5b068a8bc0bdafea97e5de7975
Bear altimeters <boris.dureau@neuf.fr>
2021-05-25 19:30:02 +0200

    fixes to the flight erase

--- RocketMotorPIDGimbal_bno055.ino
+++ RocketMotorPIDGimbal_bno055.ino
@@ -430,10 +430,10 @@ void interpretCommandBuffer(char *commandbuffer) {
 #ifdef SERIAL_DEBUG
     Serial1.println(F("Erase\n"));
 #endif
-    /*logger.clearFlightList();
+    logger.clearFlightList();
     logger.writeFlightList();
     currentFileNbr = 0;
-    currentMemaddress = 201;*/
+    currentMemaddress = 201;
 
     resetFlight();
   }
